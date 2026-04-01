9ab057bdffffa3ecfc279972815109f5a2b01f5d
Rushikesh Gaikwad <gaikwadrushi2005@gmail.com>
2026-04-01 12:02:51 +0530

    Update devops.js

--- tests/devops/devops.js
+++ tests/devops/devops.js
@@ -1,1 +1,26 @@
+function selectTool(tool){
 
+// SAVE SELECTED TOOL
+localStorage.setItem("devopsTool", tool);
+
+// ROUTING
+if(tool === "git"){
+window.location.href = "git/git.html";
+}
+else if(tool === "docker"){
+window.location.href = "docker/docker.html";
+}
+else if(tool === "kubernetes"){
+window.location.href = "kubernetes/k8s.html";
+}
+else if(tool === "terraform"){
+window.location.href = "terraform/terraform.html";
+}
+else if(tool === "jenkins"){
+window.location.href = "jenkins/jenkins.html";
+}
+else{
+window.location.href = "all/devops-all.html";
+}
+
+}
