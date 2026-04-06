c695deaa6f60301fd2c8165231b978100a37571e
Rushikesh Gaikwad <gaikwadrushi2005@gmail.com>
2026-04-06 12:36:21 +0530

    Update jenkins.js

--- tests/devops/jenkins/jenkins.js
+++ tests/devops/jenkins/jenkins.js
@@ -81,8 +81,8 @@ questions.forEach((q,index)=>{
 
 html += `<div class="question-block">
 <h4>${index+1}.</h4>
-<pre style="background:#222;color:#0f0;padding:10px;border-radius:6px;overflow:auto;">${q.question}</pre>
-
+<pre style="background:#222;color:#0f0;padding:10px;border-radius:6px;overflow:auto;">${q.question}</pre>`;
+  
 q.options.forEach((option,i)=>{
 html += `
 <label>
